240d5494abeab302f65002be86655884233efdab
YoungHoon <nobel6018@gmail.com>
2021-06-29 16:50:49 +0900

    feat: 주문 조회 V1: Entity 직접 노출

--- src/main/java/com/cloudy/jpashop/JpashopApplication.java
+++ src/main/java/com/cloudy/jpashop/JpashopApplication.java
@@ -13,6 +13,8 @@ public class JpashopApplication {
         SpringApplication.run(JpashopApplication.class, args);
     }
 
+    // Entity를 직접 반환할 때 생기는 문제해결하기 위해서 (실무에서는 DTO 반환하기 때문에 안 쓴다)
+    // LAZY 로딩을 해서 프록시가 초기화된 즉, 데이터가 로딩된 애들만 반환된다
     @Bean
     Hibernate5Module hibernate5Module() {
         return new Hibernate5Module();
