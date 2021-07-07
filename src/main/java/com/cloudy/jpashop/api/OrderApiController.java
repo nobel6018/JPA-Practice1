ee96bee49e33ad30600c33a3d866f6d130ea4583
YoungHoon <nobel6018@gmail.com>
2021-07-07 20:46:42 +0900

    feat: 주문 조회 V2: 엔티티를 DTO로 변환

--- src/main/java/com/cloudy/jpashop/api/OrderApiController.java
+++ src/main/java/com/cloudy/jpashop/api/OrderApiController.java
@@ -1,14 +1,19 @@
 package com.cloudy.jpashop.api;
 
+import com.cloudy.jpashop.domain.Address;
 import com.cloudy.jpashop.domain.Order;
 import com.cloudy.jpashop.domain.OrderItem;
+import com.cloudy.jpashop.domain.OrderStatus;
 import com.cloudy.jpashop.repository.OrderRepository;
 import com.cloudy.jpashop.repository.OrderSearch;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,4 +33,62 @@ public class OrderApiController {
 
         return all;
     }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+            .map(o -> new OrderDto(o))
+            .collect(Collectors.toList());
+
+        return collect;
+
+    }
+
+    @Getter  // @Data 는 너무 많은 것을 해줘서 필요한 것만 하기 위해 @Getter 만
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        // (Entity 버전)
+        // Entity가 외부로 노출된다
+        // 즉 Entity에 의존된다.
+//        private List<OrderItem> orderItems;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            // (Entity 버전)
+            // 프록시 초기화
+            // 프록시 초기화 안 하면 orderItems: null 로 뜬다
+//            order.getOrderItems().stream().forEach(o -> o.getItem().getName());
+//            orderItems = order.getOrderItems();
+
+            orderItems = order.getOrderItems().stream()
+                .map(orderItem -> new OrderItemDto(orderItem))
+                .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class OrderItemDto {
+
+        private String itemName;  // 상품명
+        private int orderPrice;   // 주문 가격
+        private int count;  // 주문 수량
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
+
 }
